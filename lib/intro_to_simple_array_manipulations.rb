f4da43651e588cda4aea589c6d19a8ec477e3de1
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-18 09:09:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,14 +2,19 @@ def using_concat(array, secondarray)
   array.concat(secondarray)
 end
 
-def using_concat(array, secondarray)
-  array.concat(secondarray)
+def using_insert(array, element)
+  array[4].insert(element)
 end
 
-def using_concat(array, secondarray)
-  array.concat(secondarray)
+def using_uniq(array)
+  array.uniq 
 end
 
-def using_concat(array, secondarray)
-  array.concat(secondarray)
+def using_flatten(array)
+  array.flatten 
 end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
